54625ade11afa65c48c39205ac50203a68ba1e39
iQueFrio <73860789+iQueFrio@users.noreply.github.com>
2020-12-07 20:28:40 -0600

    Sombras y margenes agregados
    Se agrego sombras y margenes a los enemigos y a los otros animales, solo el cofre y el de la montaña no tienen eso.

---
+++ Assets/Project/Scripts/FollowShadow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowShadow : MonoBehaviour
+{
+    public GameObject follower; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = follower.transform.position;
+        transform.rotation = follower.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (follower != null)
+        {
+            transform.position = follower.transform.position;
+            transform.rotation = follower.transform.rotation;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
