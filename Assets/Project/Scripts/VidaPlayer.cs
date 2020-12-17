54e75ff86f1305e491ef093d61e4e63066b324d6
iQueFrio <73860789+iQueFrio@users.noreply.github.com>
2020-12-17 15:41:28 -0600

    Agregada barra de vida
    Tambien agregue unas orbes para regenerar vida. Pd: El gato no muere si su vida llega a 0, en el scrip VidaPlayer iria esa parte

---
+++ Assets/Project/Scripts/VidaPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class VidaPlayer : MonoBehaviour
+{
+    public float vida = 100;
+    public Image barraDeVida;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vida = Mathf.Clamp(vida, 0, 100);
+        barraDeVida.fillAmount = vida/100;
+        /*
+        //Lo intente asi pero no cargo bien el menu quiza otra forma o ver que paso con esta opcion 
+        if(vida == 0){
+            SceneManager.LoadScene("Menu");
+        }
+        */
+    }
+}
